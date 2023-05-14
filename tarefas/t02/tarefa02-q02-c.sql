f4d491c74e29fab1223859ac110110f9b95aa5e8
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-05-14 15:34:14 -0300

    respondendo a questão 2-c (#2)

---
+++ tarefas/t02/tarefa02-q02-c.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION funcionarios_acima_media_idade()
+RETURNS TABLE (nome VARCHAR(15), idade INTEGER, depto INT) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT f.nome, DATE_PART('year', age(CURRENT_DATE, f.dataNasc)) AS idade, f.depto
+    FROM funcionario f
+    INNER JOIN (
+        SELECT depto, AVG(DATE_PART('year', age(CURRENT_DATE, dataNasc))) AS media_idade
+        FROM funcionario
+        GROUP BY depto
+    ) m ON f.depto = m.depto
+    WHERE DATE_PART('year', age(CURRENT_DATE, f.dataNasc)) > m.media_idade;
+END;
+$$ LANGUAGE plpgsql;
+
