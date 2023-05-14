ae18a69c630ca5dc1a22e1f0ee4169b9ab017f85
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-05-14 15:59:11 -0300

    respondendo a questão 2-f (#2)

---
+++ tarefas/t02/tarefa02-q02-f.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE PROCEDURE exibir_equipe_projeto(cod_projeto INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    SELECT projeto.descricao AS projeto, funcionario.nome AS funcionario, departamento.sigla AS departamento
+    FROM projeto
+    INNER JOIN equipe ON projeto.equipe = equipe.codigo
+    INNER JOIN membro ON equipe.codigo = membro.codEquipe
+    INNER JOIN funcionario ON membro.codFuncionario = funcionario.codigo
+    INNER JOIN departamento ON funcionario.depto = departamento.codigo
+    WHERE projeto.codigo = cod_projeto;
+END;
+$$;
+
+-- Para chamar a procedure e exibir a equipe do projeto, basta executar o seguinte comando:
+-- CALL exibir_equipe_projeto(cod_projeto);
